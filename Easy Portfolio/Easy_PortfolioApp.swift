50f31eb5ef543a13c8f0e6fd1a595db11292c39c
Vitalii Iakushev <vsiakushev@gmail.com>
2023-05-06 10:15:47 -0700

    Initial Commit

---
+++ Easy Portfolio/Easy_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_PortfolioApp.swift
+//  Easy Portfolio
+//
+//  Created by Vitaliy Iakushev on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Easy_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
